deecf35b8d79bbf0921c250f55b5943f85393fad
Emmajackson20 <emmajacksons750@gmail.com>
2024-09-12 06:52:03 +0300

    Inputs and outputs
    Java codes

---
+++ inputs.java
@@ -0,0 +1,25 @@
+import.java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.ln);
+		// asking for user name
+		System.out.println("Enter your user name");
+		String name = sc.nextLine();
+			// asking for user date of birth
+		System.out.println("Enter your date of birth");
+		String name = sc.nextLine();
+			// asking for user gender
+		System.out.println("Enter your gender");
+		String name = sc.nextLine();
+			// asking for user email
+		System.out.println("Enter your user email");
+		String name = sc.nextLine();
+			// asking for user telephone number
+		System.out.println("Enter your telphone number");
+		String name = sc.nextLine();
+			// asking for user address
+		System.out.println("Enter your address");
+		String name = sc.nextLine();
+		
+	}
+}
---
+++ operators.java
@@ -0,0 +1,19 @@
+public class Main {
+	public static void main(String[] args) {
+		//Arithematic operators
+		int a = 10;
+		int b = 5;
+		//Addition
+		int sum = a+b;
+		//Subtraction
+		int difference= a-b;
+		// multiplication
+		int product = a*b;
+		// division
+		quotient = a/b;
+		System.out.println(sum:"+sum);
+			System.out.println(difference:"+difference);
+				System.out.println(product:"+product);
+					System.out.println(quotient:"+quotient);
+	}
+}
